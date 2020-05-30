379bec4acfb10544418d8381bee4171b634f7236
SohelRaja <rohan.nsec@gmail.com>
2020-05-30 22:46:48 +0530

    Created react project and routes added

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import './App.css';
+import NavBar from './components/Navbar';
+import Home from './components/screens/Home';
+import Signin from './components/screens/Login';
+import Signup from './components/screens/Signup';
+import Profile from './components/screens/Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
