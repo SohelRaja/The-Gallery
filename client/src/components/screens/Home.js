705c1eae9d749d6cdaeb25def422db47840a5d2a
SohelRaja <rohan.nsec@gmail.com>
2020-05-31 22:29:51 +0530

    Home page ui added

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,66 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="home">
+            <div className="card home-card">
+                <div className="home-card-heading">
+                    <div>
+                        <img src="https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=311&q=80" alt="home-profile-pic"/>
+                    </div>
+                    <dic>
+                        <h5 className="home-card-title">Sohel Raja</h5>
+                    </dic>
+                </div>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="pic" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons">favorite</i>
+                    <h6>Title</h6>
+                    <p>This is a amazing photo</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+
+            <div className="card home-card">
+                <div className="home-card-heading">
+                    <div>
+                        <img src="https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=311&q=80" alt="home-profile-pic"/>
+                    </div>
+                    <dic>
+                        <h5 className="home-card-title">Sohel Raja</h5>
+                    </dic>
+                </div>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="pic" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons">favorite</i>
+                    <h6>Title</h6>
+                    <p>This is a amazing photo</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+            <div className="card home-card">
+                <div className="home-card-heading">
+                    <div>
+                        <img src="https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=311&q=80" alt="home-profile-pic"/>
+                    </div>
+                    <dic>
+                        <h5 className="home-card-title">Sohel Raja</h5>
+                    </dic>
+                </div>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="pic" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons">favorite</i>
+                    <h6>Title</h6>
+                    <p>This is a amazing photo</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+        </div>
     );
 }
 
