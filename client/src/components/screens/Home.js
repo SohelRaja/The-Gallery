1d7f3f45758c669934f062266d0465985b30007b
SohelRaja <rohan.nsec@gmail.com>
2020-06-01 20:35:28 +0530

    Fetched post and showed to the home page

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,66 +1,47 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 
 const Home = () => {
+    const [data, setData] = useState([]);
+    useEffect(()=>{
+        fetch('/allpost',{
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            }
+        }).then(res=>res.json())
+        .then((result)=>{
+            setData(result.posts);
+        }).catch(err=>{
+            console.log(err);
+        });
+    },[]);
+
     return (
         <div className="home">
-            <div className="card home-card">
-                <div className="home-card-heading">
-                    <div>
-                        <img src="https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=311&q=80" alt="home-profile-pic"/>
-                    </div>
-                    <div>
-                        <h5 className="home-card-title">Sohel Raja</h5>
-                    </div>
-                </div>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="pic" />
-                </div>
-                <div className="card-content">
-                    <i className="material-icons">favorite</i>
-                    <h6>Title</h6>
-                    <p>This is a amazing photo</p>
-                    <input type="text" placeholder="add a comment" />
-                </div>
-            </div>
-
-            <div className="card home-card">
-                <div className="home-card-heading">
-                    <div>
-                        <img src="https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=311&q=80" alt="home-profile-pic"/>
-                    </div>
-                    <div>
-                        <h5 className="home-card-title">Sohel Raja</h5>
-                    </div>
-                </div>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="pic" />
-                </div>
-                <div className="card-content">
-                    <i className="material-icons">favorite</i>
-                    <h6>Title</h6>
-                    <p>This is a amazing photo</p>
-                    <input type="text" placeholder="add a comment" />
-                </div>
-            </div>
-            <div className="card home-card">
-                <div className="home-card-heading">
-                    <div>
-                        <img src="https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=311&q=80" alt="home-profile-pic"/>
-                    </div>
-                    <div>
-                        <h5 className="home-card-title">Sohel Raja</h5>
-                    </div>
-                </div>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="pic" />
-                </div>
-                <div className="card-content">
-                    <i className="material-icons">favorite</i>
-                    <h6>Title</h6>
-                    <p>This is a amazing photo</p>
-                    <input type="text" placeholder="add a comment" />
-                </div>
-            </div>
+            {
+                data.map(item=>{
+                    return(
+                        <div className="card home-card" key={item._id}>
+                            <div className="home-card-heading">
+                                <div>
+                                    <img src="https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=311&q=80" alt="home-profile-pic"/>
+                                </div>
+                                <div>
+                                    <h5 className="home-card-title">{item.postedBy.name}</h5>
+                                </div>
+                            </div>
+                            <div className="card-image">
+                                <img src={item.photo} alt="pic" />
+                            </div>
+                            <div className="card-content">
+                                <i className="material-icons">favorite</i>
+                                <h6>{item.title}</h6>
+                                <p>{item.body}</p>
+                                <input type="text" placeholder="add a comment" />
+                            </div>
+                        </div>
+                    );
+                })
+            }
         </div>
     );
 }
