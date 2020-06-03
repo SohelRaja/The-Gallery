020557e9b7bbd14377022f3f088f9c02b520becb
SohelRaja <rohan.nsec@gmail.com>
2020-06-03 16:15:50 +0530

    Profile pic updated to cloud

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -2,10 +2,13 @@ import React, {useEffect, useState, useContext} from 'react';
 import { Link } from 'react-router-dom';
 
 import {UserContext} from '../../App';
+import {UPLOAD_PRESET,CLOUD_NAME,BASE_URL} from '../../keys';
 
 const Profile = () => {
     const [myposts,setPosts] = useState([]);
     const {state, dispatch} = useContext(UserContext);
+    const [url,setUrl] = useState(undefined);
+    const [image,setImage] = useState("");
     useEffect(()=>{
         fetch('/mypost',{
             headers: {
@@ -19,19 +22,56 @@ const Profile = () => {
             console.log(err);
         });
     },[]);
+    useEffect(()=>{
+        if(image){
+            const data = new FormData();
+            data.append("file",image);
+            data.append("upload_preset",UPLOAD_PRESET);
+            data.append("cloud_name", CLOUD_NAME);
+            // Request To Cloudinary
+            fetch(BASE_URL, {
+                method: "post",
+                body: data
+            })
+            .then(res=>res.json())
+            .then(data => {
+                setUrl(data.url);
+                console.log(data.url)
+                localStorage.setItem("user", JSON.stringify({...state, pic: data.url}));
+                dispatch({type:"UPDATEPIC", payload:{pic: data.url}})
+            })
+            .catch(err=>{
+                console.log(err);
+            });
+        }
+    },[image]);
+    const updatePhoto = (file) => {
+        setImage(file)
+    }
     return (
         <div className="profile">
             <div className="profile-card">
-                <div>
-                    <img className="profile-pic" src={state? state.pic: "loading.."} alt="profile-pic"/>
+                <div className="profile-card-plate">
+                    <div>
+                        <img className="profile-pic" src={state? state.pic: "loading.."} alt="profile-pic"/>
+                    </div>
+                    <div className="profile-info">
+                        <h4>{state? state.name : "loading..."}</h4>
+                        <h5>{state? state.email : "loading..."}</h5>
+                        <div className="profile-sub-info">
+                            <h6>{myposts.length} posts</h6>
+                            <h6>{state? state.followers.length : "0"} followers</h6>
+                            <h6>{state? state.following.length : "0"} following</h6>
+                        </div>
+                    </div>
                 </div>
-                <div className="profile-info">
-                    <h4>{state? state.name : "loading..."}</h4>
-                    <h5>{state? state.email : "loading..."}</h5>
-                    <div className="profile-sub-info">
-                        <h6>{myposts.length} posts</h6>
-                        <h6>{state? state.followers.length : "0"} followers</h6>
-                        <h6>{state? state.following.length : "0"} following</h6>
+                <div className="file-field input-field" style={{margin: "10px"}}>
+                    <div className="btn #5e35b1 deep-purple darken-1">
+                        <span>Update Pic</span>
+                        <input type="file" onChange={(e)=>updatePhoto(e.target.files[0])} />
+                    </div>
+                    <div className="file-path-wrapper">
+                        <input className="file-path validate" type="text" />
                     </div>
                 </div>
             </div>
