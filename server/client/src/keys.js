ba5b04b3a8ebf6e5082ddd9a3e482ff30355d138
SohelRaja <rohan.nsec@gmail.com>
2020-06-15 22:38:27 +0530

    Keys.js File modified

--- server/client/src/keys.js
+++ server/client/src/keys.js
@@ -1,3 +1,17 @@
-export const UPLOAD_PRESET = "the-gallery";
-export const CLOUD_NAME = "srmthegallery";
-export const BASE_URL = "https://api.cloudinary.com/v1_1/srmthegallery/image/upload"; 
+import {UPLOAD_PRESET_DEV, CLOUD_NAME_DEV, BASE_URL_DEV} from'./dev';
+import {UPLOAD_PRESET_PROD, CLOUD_NAME_PROD, BASE_URL_PROD} from './prod';
+var preset;
+var cloudName;
+var baseUrl;
+if (process.env.NODE_ENV !== 'production') {
+    preset = UPLOAD_PRESET_DEV;
+    cloudName = CLOUD_NAME_DEV;
+    baseUrl = BASE_URL_DEV;
+}else{
+    preset = UPLOAD_PRESET_PROD;
+    cloudName = CLOUD_NAME_PROD;
+    baseUrl = BASE_URL_PROD;
+}
+export const UPLOAD_PRESET = preset;
+export const CLOUD_NAME = cloudName;
+export const BASE_URL = baseUrl;
---
+++ server/client/src/prod.js
@@ -0,0 +1,3 @@
+export const UPLOAD_PRESET_PROD = process.env.REACT_APP_UPLOAD_PRESET;
+export const CLOUD_NAME_PROD = process.env.REACT_APP_CLOUD_NAME;
+export const BASE_URL_PROD = process.env.REACT_APP_BASE_URL;
